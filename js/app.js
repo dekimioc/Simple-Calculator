8e46ae5af3d5781201c6bb5f1286b37dc6a66f0e
dekimioc <dekimioc@yahoo.com>
2019-11-07 23:05:09 +0100

    HTML construction, css style and JS file where elements are saved to variables for easier calculations

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Adding elements to variables for easier calculations
+
+const userInput = document.getElementById('input-number');
+const addBtn = document.getElementById('button-add');
+const subtractBtn = document.getElementById('button-substract');
+const multiplyBtn = document.getElementById('button-multiply');
+const divideBtn = document.getElementById('button-divide');
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
